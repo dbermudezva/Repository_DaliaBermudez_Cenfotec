a73ac3d671a20f666e6c51ef26539ca1cc5220e4
dbermudezva <dbermudezva@ucenfotec.ac.cr>
2023-02-11 12:39:35 -0600

    Ejercicio-Clase3
    Código de ejercicio de Clase 3

---
+++ Clase3-Ejercicio-Codigo/resurces/js/logica.js
@@ -0,0 +1,31 @@
+//declaración guardar o reservar un pequeño espacio en la memoria temmporal
+//variable: espacio en memoria que puede cambiar su contenido a lo largo de la ejecución
+//indica una variable global
+var title;
+//indica variable estructural
+let subtitle;
+//constante es un espacio en memoria que una vez asignado no va a cambiar su valor
+const height = 200;
+//inicializar es dar valor a un espacio resevado
+title = 'este es mi primer ejercicio de js'
+subtitle = 'que emoción!';
+
+//JS es un lenguaje no tipado
+//diferentes tipos de datos
+//string= texto
+//number=numeros
+//double=decimales
+//array=listas
+//boolean=verdadero falso
+
+
+//array
+list = ['manzana', 'pera', 'sandía']
+
+//objeto es la representación de un elemento del mundo real
+var user = {
+    name='Dalia',
+    lastname='Bermudez'
+}
+
+//JSON
